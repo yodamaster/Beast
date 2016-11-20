d0a0659144df8c124e577d0a8b4cbfb54bb82b2e
Vinnie Falco <vinnie.falco@gmail.com>
2016-11-20 07:32:41 -0500

    New HTTP interfaces (API Change):
    fix #154
    fix #265
    
    This completely replaces the HTTP parser used to read and
    parse incoming messages. The new version is faster than
    the old one, and written in fewer lines. A summary of
    changes:
    
    * basic_parser is optimized for linear buffers,
      use flat_streambuf for the best performance with
      read(), async_read(), parse(), and async_parse().
    
    * parser is now only templated on isRequest
    
    * parser constructs with a reference to the message,
      instead of owning the message as before.
    
    * parser can be constructed with a header or message
      object, and does the right thing (i.e. only parse
      the header).
    
    * The overloads of read() and async_read() which take
      just a header have been removed, since they would
      throw away important parse metadata. Use parse()
      and async_parse() instead, as a replacement.
    
    * basic_parser can be instructed to pause after reading
      the header using the pause option.
    
    * The derived class callbacks for basic_parser have
      been streamlined. All strings passed to callbacks
      are presented in their entirety, instead of being
      provided in pieces.

---
+++ test/http/design.cpp
@@ -0,0 +1,130 @@
+//
+// Copyright (c) 2013-2017 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <beast/unit_test/suite.hpp>
+
+#include <beast/core/error.hpp>
+#include <beast/http/basic_parser.hpp>
+
+namespace beast {
+namespace http {
+
+/*
+
+Parse states:
+
+- need header
+- at body
+- at body-eof
+- need chunk header
+- at chunk
+
+
+
+*/
+
+template<
+    class SyncReadStream,
+    class DynamicBuffer,
+    class Parser>
+void
+new_parse(
+    SyncReadStream& stream,
+    DynamicBuffer& dynabuf,
+    Parser& parser,
+    error_code& ec)
+{
+    static_assert(is_SyncReadStream<SyncReadStream>::value,
+        "SyncReadStream requirements not met");
+    static_assert(is_DynamicBuffer<DynamicBuffer>::value,
+        "DynamicBuffer requirements not met");
+    static_assert(is_Parser<Parser>::value,
+        "Parser requirements not met");
+    for(;;)
+    {
+        auto used =
+            parser.write(dynabuf.data(), ec);
+        if(ec)
+            return;
+        dynabuf.consume(used);
+        if(! parser.need_more())
+            break;
+        boost::optional<typename
+            DynamicBuffer::mutable_buffers_type> mb;
+        auto const size =
+            read_size_helper(dynabuf, 65536);
+        BOOST_ASSERT(size > 0);
+        try
+        {
+            mb.emplace(dynabuf.prepare(size));
+        }
+        catch(std::length_error const&)
+        {
+            ec = error::buffer_overflow;
+            return;
+        }
+        dynabuf.commit(stream.read_some(*mb, ec));
+        if(ec == boost::asio::error::eof)
+        {
+            // Caller will see eof on next read.
+            ec = {};
+            parser.write_eof(ec);
+            if(ec)
+                return;
+            BOOST_ASSERT(! parser.need_more());
+            break;
+        }
+        if(ec)
+            return;
+    }
+}
+
+template<
+    class SyncReadStream,
+    class DynamicBuffer,
+    bool isRequest,
+    class Derived>
+void
+parse_body_direct(
+    SyncReadStream& stream,
+    DynamicBuffer& dynabuf,
+    basic_parser<isRequest, Derived>& parser,
+    error_code& ec)
+{
+    switch(parser.body_style())
+    {
+    case 0: // content-length
+    {
+
+        break;
+    }
+
+    case 1: // eof
+    {
+        break;
+    }
+
+    case 2: // chunked
+    {
+        break;
+    }
+    }
+}
+
+class design_test : public beast::unit_test::suite
+{
+public:
+    void
+    run() override
+    {
+    }
+};
+
+BEAST_DEFINE_TESTSUITE(design,http,beast);
+
+} // http
+} // beast
